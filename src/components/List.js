1ae9bdde70c217b8fe9a8a919f087c3f3a9a112e
Bensully17 <bensully513@gmail.com>
2018-04-21 01:01:32 -0600

    initiated React Native

---
+++ src/components/List.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
+const cards = [
+  {
+    text: 'Ben Sullivan',
+    name: 'Ben',
+    image: require('./img1.png'),
+  },
+];
+export default class List extends Component {
+  render() {
+    return (
+      <Container>   
+        <Header />
+        <View>
+          <DeckSwiper
+            dataSource={cards}
+            renderItem={item =>
+              <Card style={{ elevation: 3 }}>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={item.image} />
+                    <Body>
+                      <Text>{item.text}</Text>
+                      <Text note>Denver, CO</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem cardBody>
+                  <Image style={{ height: 300, flex: 1 }} source={item.image} />
+                </CardItem>
+                <CardItem>
+                  <Icon name="ios-car" style={{ color: '#333' }} />
+                  <Text>{item.name}</Text>
+                </CardItem>
+              </Card>
+            }
+          />
+        </View>
+      </Container>
+    );
+  }
+}
---
+++ src/containers/App.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import List from '../components/List'
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Ski-Lift!
+        </Text>
+        <List/>
+  
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 35,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    alignSelf: 'center',
+    fontSize: 20,
+  }
+});
